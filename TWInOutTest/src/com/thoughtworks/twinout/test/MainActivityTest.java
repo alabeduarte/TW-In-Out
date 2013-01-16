bee6aab1c45d9b3a6bd27f9c7991a39873aa37d5
Alabê Duarte <alabeduarte@gmail.com>
2013-01-16 11:23:58 -0300

    Adding 'I'm In' Button on Main Activity

--- TWInOutTest/src/com/thoughtworks/twinout/test/MainActivityTest.java
+++ TWInOutTest/src/com/thoughtworks/twinout/test/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.twinout.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.thoughtworks.twinout.MainActivity;
@@ -18,7 +19,13 @@ public class MainActivityTest extends
 		TextView view = (TextView) activity.findViewById(R.id.hello);
 		assertEquals("Hello world! Ronualdo", view.getText());
 	}
-
+	
+	public void testEntryButtonName() throws Exception {
+		MainActivity activity = getActivity();
+		Button button= (Button) activity.findViewById(R.id.buttonImIn);
+		assertEquals("Im In!", button.getText());
+	}
+	
 	// public void testRegisteringIN() throws Exception {
 	// Button button = (Button)activity.findViewById(R.id.inButton);
 	// button.performClick();
