5b0bf50422696257a9fd65119da10e312cf58d29
Alabê Duarte <alabeduarte@gmail.com>
2013-01-27 23:14:52 -0300

    Move monitor rules for Instrumentation Helper

--- TWInOutTest/src/com/thoughtworks/twinout/test/instrumentation/InstrumentationHelper.java
+++ TWInOutTest/src/com/thoughtworks/twinout/test/instrumentation/InstrumentationHelper.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
 
-import com.thoughtworks.twinout.MainActivity;
-
 public class InstrumentationHelper {
 	
 	private Instrumentation instrumentation;
@@ -13,29 +11,27 @@ public class InstrumentationHelper {
 	public InstrumentationHelper(Instrumentation instrumentation) {
 		this.instrumentation = instrumentation;
 	}
-
-	public Instrumentation.ActivityMonitor createMonitor(Class<? extends Activity> clazz) {
-		return instrumentation.addMonitor(clazz.getName(), null, false);
-	}
 	
-	public void startActivitySync() {
-		Intent intent = new Intent(Intent.ACTION_MAIN);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		intent.setClassName(instrumentation.getTargetContext(), MainActivity.class.getName());
-		instrumentation.startActivitySync(intent);
-	}
-	
-	public Activity getCurrentActivity(Instrumentation.ActivityMonitor monitor) {
+	public Activity createActivityFor(Class<? extends Activity> clazz) {
+		Instrumentation.ActivityMonitor monitor = this.createMonitor(clazz);
+		this.startActivitySync(clazz);
 		return instrumentation.waitForMonitorWithTimeout(monitor, 1000);
 	}
 	
-	public Instrumentation.ActivityMonitor changeMonitor(Instrumentation.ActivityMonitor monitor, Class<? extends Activity> clazz) {
-		removeMonitor(monitor);
-		return createMonitor(clazz);
-	}
-	
-	private void removeMonitor(Instrumentation.ActivityMonitor monitor) {
-		instrumentation.removeMonitor(monitor);
+	public Activity getActivityFor(Class<? extends Activity> clazz) {
+		Instrumentation.ActivityMonitor monitor = this.createMonitor(clazz);
+		return instrumentation.waitForMonitorWithTimeout(monitor, 1000);
 	}
 
+	private Instrumentation.ActivityMonitor createMonitor(Class<? extends Activity> clazz) {
+		return instrumentation.addMonitor(clazz.getName(), null, false);
+	}
+	
+	private void startActivitySync(Class<? extends Activity> clazz) {
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.setClassName(instrumentation.getTargetContext(), clazz.getName());
+		instrumentation.startActivitySync(intent);
+	}
+	
 }
--- TWInOutTest/src/com/thoughtworks/twinout/test/pages/MainPage.java
+++ TWInOutTest/src/com/thoughtworks/twinout/test/pages/MainPage.java
@@ -12,7 +12,6 @@ public class MainPage {
 
 	private InstrumentationHelper instrumentationHelper;
 	private MainActivity activity;
-	private Instrumentation.ActivityMonitor monitor;
 	
 	public static MainPage create(Instrumentation instrumentation) {
 		return new MainPage(new InstrumentationHelper(instrumentation));
@@ -20,25 +19,18 @@ public class MainPage {
 	
 	private MainPage(InstrumentationHelper instrumentationHelper) {
 		this.instrumentationHelper = instrumentationHelper;
-		this.monitor = this.instrumentationHelper.createMonitor(MainActivity.class);
-		this.instrumentationHelper.startActivitySync();
-		this.activity = (MainActivity) this.instrumentationHelper.getCurrentActivity(monitor);
+		this.activity = (MainActivity) this.instrumentationHelper.createActivityFor(MainActivity.class);
 	}
 
 	public MainActivity getActivity() {
 		return activity;
 	}
 
-	public Instrumentation.ActivityMonitor getMonitor() {
-		return monitor;
-	}
-
 	public TimeInfoPage goToTimeInfoPage() {
 		final Button buttonEntry = (Button) activity.findViewById(R.id.buttonEntry);
-		this.monitor = this.instrumentationHelper.changeMonitor(this.monitor, TimeInfoActivity.class);
 		click(buttonEntry);
 
-		TimeInfoActivity timeInfoActivity = (TimeInfoActivity) this.instrumentationHelper.getCurrentActivity(this.monitor);
+		TimeInfoActivity timeInfoActivity = (TimeInfoActivity) this.instrumentationHelper.getActivityFor(TimeInfoActivity.class);
 		return new TimeInfoPage(timeInfoActivity);
 	}
 	
